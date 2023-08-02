e67b2ec23959e652884c8db996ecfc990b9f74b8
damiano000 <damianomirante9@gmail.com>
2023-08-02 15:27:18 +0200

    Concluso senza bonus

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         String[] nomi = {"Cristiano Ronaldo ", "Leo Messi ", "Maradona ", "Pelè", "Mario", "Luigi", "Giovanni", "Andrea", "Roberto"};
-        String[] ruolidifensivi = {"Portiere", "Difensore", "Centrocampista", "Attaccante"};
+        String[] ruoligc = {"ASA", "TD", "DE", "DC", "CM", "RM", "LM"};
+
 
         Giocatore[] giocatori = new Giocatore[11];
         Random random = new Random();
-        String nomeGiocatore= nomi[random.nextInt(nomi.length)];
-        System.out.println(nomeGiocatore);
-        int etaGiocatore = random.nextInt( 16, 40);
-        System.out.println(etaGiocatore);
 
 
+            String nomeGiocatore = nomi[random.nextInt(nomi.length)];
+            System.out.println("Nome = " + nomeGiocatore);
+            int etaGiocatore = random.nextInt(16, 40);
+            System.out.println("Eta= " + etaGiocatore);
+
+            int posizionegc = (random.nextInt(0, ruoligc.length));
+            System.out.println(ruoligc[((posizionegc))]);
 
 
-    }
-}
+        }
+
+
+        }
